abe7e0e68b98e366344aa7a4b02f8de0da092f05
Mahmud Diab <mahmud.d458@gmail.com>
2018-11-20 03:04:06 +0000

    last changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ReversePhoneService } from './reverse-phone.service' ;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'project';
+export  class  AppComponent  {
+ phone:Number;
+ results;
+
+  constructor(public _api:  ReversePhoneService){}
+
+ submit()  {
+  this._api.whitePages()
+  .subscribe((res:  any)  =>  {
+  this.results = res.results;
+ console.log(res);
+  })
+  }
 }
--- src/app/reverse-phone.service.ts
+++ src/app/reverse-phone.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { Observable} from 'rxjs/Observable';
-
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+ 
 @Injectable({
   providedIn: 'root'
-  url: string = "https://proapi.whitepages.com/3.0/phone?phone";
   
 })
 export class ReversePhoneService {
-
-  constructor(private http:HttpClient) {}
-    getData () {
-      return this.http.get(this.url)
+  url: string = "https://proapi.whitepages.com/3.0/phone?phone=";
+  api: string = "0c613d7a01c2420eae892443413d5ad4"
+  
+  phone = "3342218929";  
+  
+  constructor(private _http:HttpClient) { }
+    
+    whitePages() {
+      return this._http.get(this.url + this.phone + "&api_key=" + this.api)
   }
 }
